fe4fae349ab6c59cd28e6e30ff2c104061271cca
Robert Kirkum <robert113289@gmail.com>
2017-10-11 19:05:50 -0500

    completed build a blog assignment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash,session,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,6 +7,8 @@ app.config['DEBUG']=True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO']= True
 db = SQLAlchemy(app)
+
+app.secret_key ="pickles"
 
 
 class Blogs(db.Model):
@@ -20,15 +22,16 @@ class Blogs(db.Model):
 
 @app.route('/blog')
 def blog():
-
-
-
-    blog_posts = Blogs.query.all()
-    print("*******************************",blog_posts)
-
-
+    id = request.args.get("id")
     
-    return render_template('blog.html',title="Blog",blog_posts=blog_posts)
+    if id is not None:
+        id = int(id)
+        blog = Blogs.query.filter_by(id=id).first()
+        return render_template('singlepost.html',title="Blog",blog=blog)
+
+    if id is None:
+        blog_posts = Blogs.query.all()
+        return render_template('blog.html',title="Blog",blog_posts=blog_posts)
 
 @app.route('/newpost', methods=['POST','GET'])
 def new_post():
@@ -38,10 +41,22 @@ def new_post():
         title= request.form['new_blog_entry_title']
         post= request.form['new_blog_entry']
 
+        if title == "":
+            flash("You must enter a title for your blog.")
+            return render_template('newpost.html',new_blog_entry=post)
+        
+        if post == "":
+            flash("You must add content for your new blog entry")
+            return render_template('newpost.html',new_blog_entry_title= title)
+        
+    
         new_blog_entry = Blogs(title,post)
         db.session.add(new_blog_entry)
         db.session.commit()
-        return render_template('blog.html')
+        id = new_blog_entry.id
+        id = str(id)
+        blog = Blogs.query.filter_by(id=id).first()
+        return redirect('/blog?id=' + id)
 
     
 
